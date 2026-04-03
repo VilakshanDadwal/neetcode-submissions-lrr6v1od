7bf36c1a21a226a544c1013fba6ca04afa434d97
Vila D02 <vilakshandadwal@gmail.com>
2026-04-03 14:18:20 +0530

    Add: coin-change-ii - submission-4

---
+++ Data Structures & Algorithms/coin-change-ii/submission-4.java
@@ -0,0 +1,17 @@
+// bottom up revision - starting from 0,0. single array.
+class Solution {
+    public int change(int amount, int[] coins) {
+        var dp = new int[amount+1];
+
+        dp[0] = 1;
+
+        for(int i=1; i<=coins.length; i++) {
+            for(int target=1; target<=amount; target++) {
+                if(coins[i-1] <= target) {
+                    dp[target] = dp[target] + dp[target - coins[i-1]];
+                }
+            }
+        }
+        return dp[amount];
+    }
+}
