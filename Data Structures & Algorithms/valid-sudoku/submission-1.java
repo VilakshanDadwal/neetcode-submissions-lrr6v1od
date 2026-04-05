7c7774fe3e6cbb7fa4f8e44662ca00de7a937736
Vila D02 <vilakshandadwal@gmail.com>
2026-04-05 14:54:44 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        var rowSeen = new ArrayList<Set<Character>>();
+        var colSeen = new ArrayList<Set<Character>>();
+        var boxSeen = new ArrayList<Set<Character>>();
+        for(int i=0; i< board.length; i++) {
+            rowSeen.add(new HashSet<Character>());
+            colSeen.add(new HashSet<Character>());
+            boxSeen.add(new HashSet<Character>());
+        }
+
+        for(int i=0; i<board.length; i++) {
+            for(int j=0; j<board[0].length; j++) {     
+                var currentChar = board[i][j];           
+                if(currentChar == '.') continue;
+                // Representing boxes as a single index in a list 
+                var boxIdx = i/3 * 3 + j/3;
+                if(rowSeen.get(i).contains(currentChar) 
+                    || colSeen.get(j).contains(currentChar) 
+                    || boxSeen.get(boxIdx).contains(currentChar))
+                        return false;
+                rowSeen.get(i).add(currentChar);
+                colSeen.get(j).add(currentChar);
+                boxSeen.get(boxIdx).add(currentChar);
+            }
+        }
+        return true;
+    }
+}
