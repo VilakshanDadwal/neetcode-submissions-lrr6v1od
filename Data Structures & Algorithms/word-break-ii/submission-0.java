df56ae3634854844e25fa03315ac95aecce880e3
Vila D02 <vilakshandadwal@gmail.com>
2026-05-18 16:34:44 +0530

    Add: word-break-ii - submission-0

---
+++ Data Structures & Algorithms/word-break-ii/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        var words = new HashSet<String>();
+        for(var word: wordDict) {
+            words.add(word);
+        }
+
+        var result = new ArrayList<String>();
+        wordBreak(0, s, words, new ArrayList<String>(), result);
+        return result;
+    }
+
+    void wordBreak(int idx, String s, Set<String> words, List<String> current, List<String> result) {
+        if(idx == s.length())
+            result.add(String.join(" ", current));
+        for(int i=idx; i<s.length(); i++) {
+            
+            if(!words.contains(s.substring(idx, i+1))) continue;
+
+            current.add(s.substring(idx, i+1));
+            wordBreak(i+1, s, words, current, result);
+        }
+        if(current.size()>0) current.remove(current.size()-1);
+    }
+}
