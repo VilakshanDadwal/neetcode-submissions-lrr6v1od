74f390636f80677130383d3b7810571cd87106cf
Vila D02 <vilakshandadwal@gmail.com>
2026-05-31 14:10:43 +0530

    Add: longest-substring-without-duplicates - submission-12

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-12.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        var freq = new HashMap<Character, Integer>();
+        var maxLength = 0;
+        var left = 0;
+        for(int right=0; right<s.length(); right++) {
+            var currentChar = s.charAt(right);
+            if(!freq.containsKey(currentChar)) {
+                maxLength = Math.max(maxLength, right - left + 1);
+            } else if(freq.get(currentChar) >= left) {
+                left = freq.get(currentChar) + 1;
+            }
+            maxLength = Math.max(maxLength, right - left + 1);
+            freq.put(currentChar, right);
+        }
+        return maxLength;
+    }
+}
