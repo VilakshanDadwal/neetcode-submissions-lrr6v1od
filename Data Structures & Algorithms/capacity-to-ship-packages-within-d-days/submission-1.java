0c477ee6a0d20c66d21445680348751040c8220d
Vila D02 <vilakshandadwal@gmail.com>
2026-06-18 15:55:44 +0530

    Add: capacity-to-ship-packages-within-d-days - submission-1

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        var minPossibleCapacity = Arrays.stream(weights).max().orElse(0);
+        var maxPossibleCapacity = Arrays.stream(weights).sum();
+        for (int capacity = minPossibleCapacity; capacity <= maxPossibleCapacity; capacity++) {
+            var weightSum = 0;
+            var daysNeeded = 1;
+            for (int weight : weights) {
+                weightSum += weight;
+                if (weightSum > capacity) {
+                    daysNeeded++;
+                    weightSum = weight;
+                }
+            }
+            if (daysNeeded <= days) {
+               return capacity;
+            }
+        }
+
+        return -1;
+    }
+}
