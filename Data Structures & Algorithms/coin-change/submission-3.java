d9db069418ad3764ba857aa85af1355b75e90082
Vila D02 <vilakshandadwal@gmail.com>
2026-07-13 12:23:40 +0530

    Update: coin-change - submission-3

--- Data Structures & Algorithms/coin-change/submission-3.java
+++ Data Structures & Algorithms/coin-change/submission-3.java
@@ -1,26 +1,19 @@
+// DP solution
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        var cache = new HashMap<Integer, Integer>();
-        cache.put(0,0);
-        int minCoins = minCoins(coins, amount, cache);
-        return minCoins == Integer.MAX_VALUE ? -1 : minCoins;
-    }
-    int minCoins(int[] coins, int amount, Map<Integer, Integer> cache) {
-        if(amount==0) return 0;
-        if(cache.containsKey(amount)) return cache.get(amount);
 
-        int minCoins = Integer.MAX_VALUE;
-        for(int coin: coins) {
-            if(amount-coin >= 0) {
-                int coinChange = minCoins(coins, amount - coin, cache);
-                if(coinChange == Integer.MAX_VALUE) {
-                    minCoins = Math.min(minCoins, coinChange);
-                } else {
-                    minCoins = Math.min(minCoins, 1 + coinChange);
+    public int coinChange(int[] coins, int amount) {
+        var current = new int[amount+1];
+        Arrays.fill(current, amount+1);
+        current[0] = 0;
+
+        for(int i=0; i<coins.length; i++) {
+            for(int j=0; j<= amount; j++) {
+                var currentCoin = coins[i];
+                if(currentCoin <= j) {
+                    current[j] = Math.min(current[j], 1 + current[j - currentCoin]);
                 }
             }
         }
-        cache.put(amount, minCoins);
-        return minCoins;
+        return current[amount] == amount+1 ? -1 : current[amount];
     }
 }
